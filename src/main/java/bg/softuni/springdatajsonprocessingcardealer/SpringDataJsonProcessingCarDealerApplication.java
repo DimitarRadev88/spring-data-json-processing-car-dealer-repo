b29be556f283250f59f8932f6c29f4f0c6db0470
dimit <dimitar_rradev@abv.bg>
2024-12-12 11:53:45 +0200

    first commit

---
+++ src/main/java/bg/softuni/springdatajsonprocessingcardealer/SpringDataJsonProcessingCarDealerApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.springdatajsonprocessingcardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJsonProcessingCarDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJsonProcessingCarDealerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/springdatajsonprocessingcardealer/SpringDataJsonProcessingCarDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.springdatajsonprocessingcardealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJsonProcessingCarDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
